cbf569e3b9a04ec68356b5b269fe5b74ca5a4a41
Aki <xiaolingaki@gmail.com>
2023-05-31 15:47:43 +0800

    10

--- chataki/urls.py
+++ chataki/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     
     path("process/", views.process, name="process"),
+    path("processgpt/",views.process_gpt,name="processgpt"),
     path("index/",views.index,name='index'),
     path("test/",views.chat_test,name='chat_test')
     
